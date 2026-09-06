db37ccf7377138e69813cb5232c608ca35f929d4
mafcastroc7 <castro.mf7@gmail.com>
2026-09-05 22:00:00 -0500

    Add generic repository, match repository and database seeder (Module 3)

---
+++ GolBet.Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,28 @@
+﻿// GolBet.Repositories/Interfaces/IGenericRepository.cs
+using GolBet.Entities.Common;
+
+namespace GolBet.Repositories.Interfaces;
+
+/// <summary>
+/// Generic data-access contract for all domain entities.
+/// Specific queries live in entity-specific repositories.
+/// </summary>
+public interface IGenericRepository<T> where T : AuditableEntity
+{
+    // ---- Queries ---- CQS
+    //METODO 1
+    Task<IEnumerable<T>> GetAllAsync(bool includeInactive = false);
+
+    //METODO 2
+    Task<T?> GetByIdAsync(int id);
+
+    // ---- Commands ----
+    //METODO 3
+    Task<T> AddAsync(T entity);
+
+    //METODO 4
+    Task UpdateAsync(T entity);
+
+    //METODO 5
+    Task DeactivateAsync(int id);   // logical delete: IsActive = false
+}
